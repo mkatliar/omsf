500e0be2fb0266ddb9f32283d33d59b724e9dfc6
Mikhail Katliar <cepstr@gmail.com>
2019-04-05 01:10:53 +0200

    Added casadi to dependencies

--- setup.py
+++ setup.py
@@ -145,6 +145,7 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
+        'casadi',
         'control',
         'scipy',
         'matplotlib',
