386fee179c9b9a550b378ed1f55508ab4d8a3d4a
Mikhail Katliar <cepstr@gmail.com>
2022-01-30 22:32:58 +0100

    Fixed an error in test_vectorInterpolant() that appeared with updated casadi and numpy.

--- omsf_test/test_math.py
+++ omsf_test/test_math.py
@@ -1,19 +1,19 @@
 ##
 ## Copyright (c) 2015-2018 Mikhail Katliar, Max Planck Institute for Biological Cybernetics.
-## 
-## This file is part of Offline Motion Simulation Framework (OMSF) 
+##
+## This file is part of Offline Motion Simulation Framework (OMSF)
 ## (see https://github.com/mkatliar/omsf).
-## 
+##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU Lesser General Public License as published by
 ## the Free Software Foundation, either version 3 of the License, or
 ## (at your option) any later version.
-## 
+##
 ## This program is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ## GNU Lesser General Public License for more details.
-## 
+##
 ## You should have received a copy of the GNU Lesser General Public License
 ## along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
@@ -30,9 +30,9 @@ class Test(unittest.TestCase):
 
 
     def test_quatR(self):
-        q = np.array([1, 0, 0, 0]);        
+        q = np.array([1, 0, 0, 0])
         R = np.eye(3, 3)
-        
+
         numpy.testing.assert_allclose(omsf.math.quatR(q), R)
 
 
@@ -56,7 +56,7 @@ class Test(unittest.TestCase):
 
         for xq in np.linspace(min(x), max(x), 10 * M):
             for vii in vi:
-                numpy.testing.assert_equal(vii(xq), np.atleast_2d([sii(xq) for sii in si]).T)
+                numpy.testing.assert_equal(vii(xq), np.atleast_2d([float(sii(xq)) for sii in si]).T)
 
 
 if __name__ == "__main__":
